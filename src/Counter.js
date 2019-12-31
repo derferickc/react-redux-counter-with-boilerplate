12bc7b247a85f5e6cda4b6e0ea5ccf432eae777a
Frederick Choe <frederick@glamhive.com>
2019-12-31 01:24:26 -0800

    first commit

---
+++ src/Counter.js
@@ -0,0 +1,39 @@
+import React from 'react'
+
+class Counter extends React.Component {
+	constructor(props) {
+		super(props)
+
+		this.state = {
+			count: 0
+		}
+	}
+
+	increment = () => {
+		this.setState({
+			count: this.state.count +1
+		})
+	}
+
+	decrement = () => {
+		this.setState({
+			count: this.state.count -1
+		})
+	}
+
+	render() {
+		return(
+			<div className="counter">
+				<h2>Counter</h2>
+				<div>
+					<button onClick={this.decrement}>&ndash;</button>
+					<span className="count">{this.state.count}</span>
+					<button onClick={this.increment}>+</button>
+				</div>
+				
+			</div>
+		)
+	}
+}
+
+export default Counter
