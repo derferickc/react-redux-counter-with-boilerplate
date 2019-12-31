5319a515c15a4d26f4eee6130966046e4ee848c2
Frederick Choe <frederick@glamhive.com>
2019-12-31 02:41:12 -0800

    reducers and components compartmentalized

--- src/App.js
+++ src/App.js
@@ -1,28 +1,7 @@
 import React from 'react';
-import Counter from './Counter'
-import { createStore } from 'redux'
+import Counter from './components/Counter'
 import { Provider } from 'react-redux'
-
-const initialState = {
-  count: 0
-}
-
-function reducer(state = initialState, action) {
-  switch(action.type) {
-    case "INCREMENT":
-      return {
-        count: state.count + 1
-      }
-    case "DECREMENT":
-      return {
-        count: state.count - 1
-      }
-    default:
-      return state
-  }
-}
-
-const store = createStore(reducer);
+import store from './store';
 
 function App() {
   return (
