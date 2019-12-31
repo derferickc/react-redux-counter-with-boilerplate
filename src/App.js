9b65113771006278a50bddff4e18f98988799414
Frederick Choe <frederick@glamhive.com>
2019-12-31 01:59:56 -0800

    redux boilerplate configured

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
 import React from 'react';
 import Counter from './Counter'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+
+const initialState = {
+  count: 0
+}
+
+function reducer(state = initialState, action) {
+  switch(action.type) {
+    case "INCREMENT":
+      return {
+        count: state.count + 1
+      }
+    case "DECREMENT":
+      return {
+        count: state.count - 1
+      }
+    default:
+      return state
+  }
+}
+
+const store = createStore(reducer);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Counter />
-      </header>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <header className="App-header">
+          <Counter />
+        </header>
+      </div>
+    </Provider>
   );
 }
 
--- src/Counter.js
+++ src/Counter.js
@@ -1,24 +1,14 @@
 import React from 'react'
+import { connect } from 'react-redux'
 
 class Counter extends React.Component {
-	constructor(props) {
-		super(props)
-
-		this.state = {
-			count: 0
-		}
-	}
 
 	increment = () => {
-		this.setState({
-			count: this.state.count +1
-		})
+		this.props.dispatch({ type: "INCREMENT" })
 	}
 
 	decrement = () => {
-		this.setState({
-			count: this.state.count -1
-		})
+		this.props.dispatch({ type: "DECREMENT" })
 	}
 
 	render() {
@@ -27,7 +17,7 @@ class Counter extends React.Component {
 				<h2>Counter</h2>
 				<div>
 					<button onClick={this.decrement}>&ndash;</button>
-					<span className="count">{this.state.count}</span>
+					<span className="count">{this.props.count}</span>
 					<button onClick={this.increment}>+</button>
 				</div>
 				
@@ -36,4 +26,8 @@ class Counter extends React.Component {
 	}
 }
 
-export default Counter
+const mapStateToProps = (state) => ({
+	count: state.count
+})
+
+export default connect(mapStateToProps)(Counter)
